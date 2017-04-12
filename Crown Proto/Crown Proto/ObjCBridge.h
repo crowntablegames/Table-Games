8f7cd7e2f40d3234c45ecafbb9d4c3c0ece11093
rmit-s3459784-Campbell-Brobbel <s3459784@student.rmit.edu.au>
2017-04-12 15:11:32 +1000

    Cocoa

---
+++ Crown Proto/Crown Proto/ObjCBridge.h
@@ -0,0 +1,14 @@
+//
+//  ObjCBridge.h
+//  Crown Proto
+//
+//  Created by Campbell Brobbel on 12/4/17.
+//  Copyright © 2017 Campbell Brobbel. All rights reserved.
+//
+
+#import <EstimoteSDK/EstimoteSDK.h>
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+
+#endif /* ObjCBridge_h */
