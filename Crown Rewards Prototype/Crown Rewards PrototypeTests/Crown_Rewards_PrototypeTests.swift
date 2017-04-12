9659785c7c18fe5b820e3657c7d37c5c49d60721
rmit-s3459784-Campbell-Brobbel <s3459784@student.rmit.edu.au>
2017-04-12 12:28:39 +1000

    Commit 1

---
+++ Crown Rewards Prototype/Crown Rewards PrototypeTests/Crown_Rewards_PrototypeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Crown_Rewards_PrototypeTests.swift
+//  Crown Rewards PrototypeTests
+//
+//  Created by Campbell Brobbel on 12/4/17.
+//  Copyright © 2017 Campbell Brobbel. All rights reserved.
+//
+
+import XCTest
+@testable import Crown_Rewards_Prototype
+
+class Crown_Rewards_PrototypeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
