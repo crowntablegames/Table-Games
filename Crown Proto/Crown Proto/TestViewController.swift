fdbbbbcb3f09c00b00f0373de6398c56f771bb82
rmit-s3459784-Campbell-Brobbel <s3459784@student.rmit.edu.au>
2017-04-13 16:18:52 +1000

    Notification working

--- Crown Proto/Crown Proto/TestViewController.swift
+++ Crown Proto/Crown Proto/TestViewController.swift
@@ -18,6 +18,8 @@ class TestViewController: UIViewController, CLLocationManagerDelegate {
     var beaconManager : CLLocationManager = CLLocationManager()
     let proxID : String = "B9407F30-F5F8-466E-AFF9-25556B57FE6E"
     var tableBeacon : CLBeacon?
+    
+    var tableInRange = false
     var playerAtTable : Bool = false
     var tables : [Table] = []
     
@@ -33,7 +35,6 @@ class TestViewController: UIViewController, CLLocationManagerDelegate {
 
         let region : CLBeaconRegion = CLBeaconRegion(proximityUUID: UUID(uuidString: proxID)!, identifier: "TEST")
         beaconManager.startRangingBeacons(in: region)
-        
 
     }
     
@@ -44,28 +45,28 @@ class TestViewController: UIViewController, CLLocationManagerDelegate {
         
         if(knownBeacons.count > 0) {
             
-            for b in knownBeacons {
-                print(b)
-            }
-            print("-------")
-            
             if (tableBeacon != nil) {
-                timerFunc()
+    
                 if (!playerAtTable) {
+                    timerFunc()
                     if (seconds == 10) {
                         //Compare the stored Beacon with the first beacon in the array
                         //If beacon is the same as beacons[0] then player is now at the table.
                         
-                        
                         if (tableBeacon!.major == knownBeacons.first!.major && tableBeacon!.minor == knownBeacons.first!.minor) {
                             print("Player is at table")
+                            print(tableBeacon)
+                            print("---------")
                             playerAtTable = true
                             seconds = 0
+                            timerLabel.text = "\(seconds)"
                         }
                         else {
                             print("Table Not Connected")
                             tableBeacon = nil
                             seconds = 0
+                            timerLabel.text = "\(seconds)"
+
                         }
                     
                     }
@@ -74,16 +75,57 @@ class TestViewController: UIViewController, CLLocationManagerDelegate {
                     }
                 }
                 else {
+                    // Player is at the table
                     
+                    if(beacons.first!.major == tableBeacon!.major && beacons.first!.minor == tableBeacon!.minor) {
+                            tableInRange = true
+                            seconds = 0
+                            timerLabel.text = "\(seconds)"
+                            print("Player Still at table")
+                         // Player is still at table
+                            
+                    }
+                    else {
+                        tableInRange = false;
+                    }
                     
+                    if(!tableInRange) {
+                        print("Table going out of range")
+                        timerFunc()
+                        
+                            if(beacons.first!.major == tableBeacon!.major && beacons.first!.minor == tableBeacon!.minor) {
+                                tableInRange = true
+                                seconds = 0
+                                timerLabel.text = "\(seconds)"
+                                print("Player Back at table")
+                                
+                                // Player is still at table
+                                
+                            }
+                            else if(seconds == 10){
+                                print("Player Left Table")
+                                tableBeacon = nil
+                                seconds = 0
+                                timerLabel.text = "\(seconds)"
+                                playerAtTable = false
+                                tableBeacon = nil
+                                playerDidLeaveTable()
+                            }
+                            else {
+                                //Do Nothing
+                            }
+                        
+                    }
+                    else {
+                        // Table is still the first on the list
+                    }
                 }
-            
                 
                 // Otherwise continue Ranging
             }
             else {
                 tableBeacon = beacons.first! as CLBeacon
-               
+
             }
             
         }
@@ -105,8 +147,11 @@ class TestViewController: UIViewController, CLLocationManagerDelegate {
     }
     func setupTables() -> Void {
         
+        //Candy
         let table1 = Table(tableNumber: "2412", dealerName: "Campbell", major: 48476, minor: 56089)
+        //Lemon
         let table2 = Table(tableNumber: "2413", dealerName: "Seth", major: 48476, minor: 28165)
+        
         
         tables.append(table1)
         tables.append(table2)
@@ -114,8 +159,8 @@ class TestViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func timerFunc() -> Void {
-        timerLabel.text = seconds.description
         seconds += 1
+        timerLabel.text = seconds.description
     }
 
    public func playerDidLeaveTable() {
@@ -128,7 +173,7 @@ class TestViewController: UIViewController, CLLocationManagerDelegate {
         content.categoryIdentifier = "exitRegionNotification"
         
         // Deliver the notification in five seconds.
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0, repeats: false)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
         let request = UNNotificationRequest(identifier: "finishRedeemingPoints", content: content, trigger: trigger)
         let center = UNUserNotificationCenter.current()
         
