d13c6c59260c03b231e0fa2f6f7e3e05283625e1
rmit-s3459784-Campbell-Brobbel <s3459784@student.rmit.edu.au>
2017-04-18 13:27:36 +1000

    Commit

--- Crown Proto/Crown Proto/SurveyViewController.swift
+++ Crown Proto/Crown Proto/SurveyViewController.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
-class SurveyViewController: UIViewController, UITextFieldDelegate {
+class SurveyViewController: UIViewController, UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let positiveFbackArray : [String] = ["They paid me a lot of money", "They were friendly", "Game pace was excellent", "Great business understanding", "Other"]
+
+    let negativeFbackArray : [String] = ["They took my money", "They were unfriendly", "Game pace was too fast/slow", "Lacking business knowledge", "Other"]
+    
     @IBOutlet weak var textField: UITextField!
 
     @IBOutlet var pickerView: UIPickerView!
@@ -18,16 +23,27 @@ class SurveyViewController: UIViewController, UITextFieldDelegate {
         
         let rounded : Int = Int(slider.value)
         slider.value = Float(rounded)
+        pickerView.reloadAllComponents()
         
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupPickerView()
         // Do any additional setup after loading the view.
        textField.delegate = self
         
     }
-
+    
+    func setupPickerView() {
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        pickerView.layer.cornerRadius = 10
+        
+    }
+    func updatePickerView() {
+        
+    }
     @IBAction func dismiss(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
 
@@ -47,7 +63,36 @@ class SurveyViewController: UIViewController, UITextFieldDelegate {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-
+    
+  
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    
+    // returns the # of rows in each component..
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        var numberOfRows : Int
+        if slider.value < 3 {
+            numberOfRows = negativeFbackArray.count
+        }
+        else {
+            numberOfRows = positiveFbackArray.count
+        }
+        return numberOfRows
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        var title : String
+        
+        if slider.value < 3 {
+            title = negativeFbackArray[row]
+        }
+        else {
+            title = positiveFbackArray[row]
+        }
+        return title
+    }
 
 }
 
